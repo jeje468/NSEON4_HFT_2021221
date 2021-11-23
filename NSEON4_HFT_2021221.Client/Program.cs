005bc9290a779ae41e95aeef252c5e77a790d3c2
Bene Máté <mate.bene0502@gmail.com>
2021-11-23 21:01:36 +0100

    First 3 menu points created

--- NSEON4_HFT_2021221.Client/Program.cs
+++ NSEON4_HFT_2021221.Client/Program.cs
@@ -1,8 +1,6 @@
-﻿using NSEON4_HFT_2021221.Data;
-using NSEON4_HFT_2021221.Logic;
-using NSEON4_HFT_2021221.Models;
-using NSEON4_HFT_2021221.Repository;
+﻿using NSEON4_HFT_2021221.Models;
 using System;
+using System.Collections.Generic;
 
 namespace NSEON4_HFT_2021221.Client
 {
@@ -10,29 +8,194 @@ namespace NSEON4_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            PhoneDbContext db = new PhoneDbContext();
+            System.Threading.Thread.Sleep(8000);
 
-            PhoneLogic phoneLogic = new PhoneLogic(new PhoneRepository(db));
+            RestService rest = new RestService("http://localhost:62814");
 
-            BrandLogic brandLogic = new BrandLogic(new BrandRepository(db));
+            //rest.Post<Brand>(new Brand() { Name = "Google" }, "brand");
 
-            HeadquarterLogic headquarterLogic = new HeadquarterLogic(new HeadquarterRepository(db));
+            //var brands = rest.Get<Brand>("brand");
+            //var phones = rest.Get<Phone>("phone");
+            //var headquarters = rest.Get<Headquarter>("headquarter");
+            //var countries = rest.Get<Country>("country");
 
-            CountryLogic countryLogic = new CountryLogic(new CountryRepository(db));
+            //var q1 = rest.Get<KeyValuePair<string, int>>("stat/numberofphonesbybrands");
 
-            var q = headquarterLogic.ReadAll();
+            //var q2 = rest.Get<KeyValuePair<string, string>>("stat/bestcamerabyeachbrand");
 
-            var q2 = phoneLogic.NumberOfPhonesByBrands();
+            //var q3 = rest.Get<KeyValuePair<string, int>>("stat/brandwiththemostexpensivephone");
 
-            var q3 = countryLogic.CountryWithMostHeadquarters();
+            //var q4 = rest.Get<KeyValuePair<string, int>>("stat/countrywithmostheadquarters");
 
-            var q4 = countryLogic.CountriesAndBrandsThatManufactureThere();
+            //var q5 = rest.Get<KeyValuePair<string, IEnumerable<string>>>("stat/countriesandbrandsthatmanufacturethere");
 
-            var q5 = brandLogic.BestCameraByEachBrand();
+            int num;
 
-            var q6 = brandLogic.BrandWithTheMostExpensivePhone();
+            Console.WriteLine("Welcome to the mobile phone database! Please select one of the options below!");
+            do
+            {
+                Console.WriteLine(
+                    "\n0. Finish\n" +
+                    "1. Display brands/phones/countries/headquarters\n" +
+                    "2. Diplay extra info\n" +
+                    "3. Create new brand/phone/country/headquarter\n"
+                    );
+
+                num = int.Parse(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        Display();                        
+                        break;                    
+                    case 2:
+                        DisplayExtraInfo();
+                        break;;
+                    case 3:
+                        Create();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while(num != 0)
+                
 
             ;
+
+            void Display()
+            {
+                Console.WriteLine(
+                    "What do you wish to display?\n" +
+                    "1. Brands\n" +
+                    "2. Phones\n" +
+                    "3. Countries\n" +
+                    "4. Headquarters\n");
+                
+                int num = int.Parse(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        var brands = rest.Get<Brand>("brand");
+                        Recorder<Brand> brandRecorder = new Recorder<Brand>(brands);
+                        brandRecorder.Write();
+                        break;
+                    case 2:
+                        var phones = rest.Get<Phone>("phone");
+                        Recorder<Phone> phoneRecorder = new Recorder<Phone>(phones);
+                        phoneRecorder.Write();
+                        break;
+                    case 3:
+                        var countries = rest.Get<Country>("country");
+                        Recorder<Country> countryRecorder = new Recorder<Country>(countries);
+                        countryRecorder.Write();
+                        break;
+                    case 4:
+                        var headquarters = rest.Get<Headquarter>("headquarter");
+                        Recorder<Headquarter> headquarterRecorder = new Recorder<Headquarter>(headquarters);
+                        headquarterRecorder.Write();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            void DisplayExtraInfo()
+            {
+                Console.WriteLine(
+                    "1. The number of phones manufactured by each brand\n" +
+                    "2. The best camera by each brand\n" +
+                    "3. The brand with the most expensive phone\n" +
+                    "4. The country with the most headquarters\n" +
+                    "5. The countries and brands that manufacture there\n");
+
+                int num = int.Parse(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        var q1 = rest.Get<KeyValuePair<string, int>>("stat/numberofphonesbybrands");
+                        foreach (var item in q1)
+                        {
+                            Console.WriteLine("Brand: " + item.Key + "\t Number: " + item.Value.ToString());
+                        }
+                        break;
+                    case 2:
+                        var q2 = rest.Get<KeyValuePair<string, string>>("stat/bestcamerabyeachbrand");
+                        foreach (var item in q2)
+                        {
+                            Console.WriteLine("Brand: " + item.Key + "\t Phone: " + item.Value);
+                        }
+                        break;
+                    case 3:
+                        var q3 = rest.Get<KeyValuePair<string, int>>("stat/brandwiththemostexpensivephone");
+                        foreach (var item in q3)
+                        {
+                            Console.WriteLine("Brand: " + item.Key + "\t Phone: " + item.Value.ToString());
+                        }
+                        break;
+                    case 4:
+                        var q4 = rest.Get<KeyValuePair<string, int>>("stat/countrywithmostheadquarters");
+                        foreach (var item in q4)
+                        {
+                            Console.WriteLine("Country: " + item.Key + "\t Number: " + item.Value.ToString());
+                        }
+                        break;
+                    case 5:
+                        var q5 = rest.Get<KeyValuePair<string, IEnumerable<string>>>("stat/countriesandbrandsthatmanufacturethere");
+                        foreach (var item in q5)
+                        {
+                            Console.WriteLine("Country: " + item.Key);
+                            foreach (var brand in item.Value)
+                            {
+                                Console.WriteLine("-" + brand);
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            void Create()
+            {
+                Console.WriteLine(
+                    "What do you wish to create?\n" +
+                    "1. Brand\n" +
+                    "2. Phone\n" +
+                    "3. Country\n" +
+                    "4. Headquarter\n");
+
+                int num = int.Parse(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        var brands = rest.Get<Brand>("brand");
+                        Brand newBrand = new Recorder<Brand>(brands).Create();
+                        rest.Post<Brand>(newBrand, "brand");
+                        break;
+                    case 2:
+                        var phones = rest.Get<Phone>("phone");
+                        Phone newPhone = new Recorder<Phone>(phones).Create();
+                        rest.Post<Phone>(newPhone, "phone");
+                        break;
+                    case 3:
+                        var countries = rest.Get<Country>("country");
+                        Country newCountry = new Recorder<Country>(countries).Create();
+                        rest.Post<Country>(newCountry, "country");
+                        break;
+                    case 4:
+                        var headquarters = rest.Get<Headquarter>("headquarter");
+                        Headquarter newHeadquarter = new Recorder<Headquarter>(headquarters).Create();
+                        rest.Post<Headquarter>(newHeadquarter, "headquarter");
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+
+        
     }
 }
---
+++ NSEON4_HFT_2021221.Client/Recorder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSEON4_HFT_2021221.Client
+{
+    class Recorder<T>
+    {
+        List<T> items;
+
+        public Recorder(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public T Create()
+        {
+            Type t = typeof(T);
+            var props = t.GetProperties();
+            var obj = Activator.CreateInstance(t);
+
+            foreach (var prop in props)
+            {
+                if ((prop.PropertyType.Name.Contains("Int") || prop.PropertyType.Name.Contains("String")) && !prop.Name.Equals("Id"))
+                {
+                    Console.WriteLine("Enter " + prop.Name + ":");
+
+                    string value = Console.ReadLine();
+
+                    var proptype = prop.PropertyType;
+                    var methods = proptype.GetMethods();
+                    var parser = methods.FirstOrDefault(t => t.Name == "Parse");
+
+                    if (parser != null)
+                    {
+                        var converted = parser
+                            .Invoke(null, new[] { value });
+                        prop.SetValue(obj, converted);
+                    }
+                    else
+                    {
+                        prop.SetValue(obj, value);
+                    }
+                }
+            }              
+
+            return (T)obj;
+        }
+
+        public void Write()
+        {
+            Type t = typeof(T);
+                        
+            var props = t.GetProperties();
+
+            foreach (var item in items)
+            {
+                foreach (var prop in t.GetProperties())
+                {
+                    if (prop.Name.Equals("Id") || prop.Name.Equals("Name") || prop.Name.Equals("City"))
+                    {
+                        Console.WriteLine(prop.Name + ": " + prop.GetValue(item));
+                    }
+                }
+            }                        
+        }
+    }
+}
